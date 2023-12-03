f0eabe601caf5e96b186b7cadd6cc02841ea6431
xujailiu <xujialiuphd@gmail.com>
2023-12-03 10:39:15 +0800

    update test-hello.R file

--- tests/testthat/test-hello.R
+++ tests/testthat/test-hello.R
@@ -2,27 +2,8 @@ library(testthat)
 library(lrp)
 
 
-# 示例函数
-create_summary <- function(data) {
-  summary_df <- data.frame(
-    Mean = mean(data),
-    Median = median(data),
-    SD = sd(data)
-  )
-  return(summary_df)
-}
-
-# 测试数据
-test_data <- 1:10
-
-# 测试
-testthat::test_that("create_summary returns the expected results", {
-  result <- create_summary(test_data)
-  testthat::expect_snapshot_value(result, style = "deparse")
-  testthat::expect_snapshot_value(result, style = "deparse")
-})
-
-
-
-
-
+test_that(
+  "hello output should be string",{
+    expect_output(hello('xujialiu'), "Hello, world! xujialiu")
+  }
+)
